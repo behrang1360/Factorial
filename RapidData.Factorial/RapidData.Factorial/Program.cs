0185d2567e9854bc415def39bdbe043eca5bbe13
Behzad Bagherian <behrang1360@gmail.com>
2021-11-17 09:07:20 +0100

    initilize project

---
+++ RapidData.Factorial/RapidData.Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RapidData.Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
