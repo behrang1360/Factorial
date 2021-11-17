a6ebe611c19e3e32fb8f9b15882cea1d501633dd
Behzad Bagherian <behrang1360@gmail.com>
2021-11-17 15:59:53 +0100

    merged

--- RapidData.Factorial/RapidData.Factorial/Program.cs
+++ RapidData.Factorial/RapidData.Factorial/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Numerics;
 
 namespace RapidData.Factorial
 {
@@ -10,19 +7,9 @@ namespace RapidData.Factorial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-
-            MathLib mathLib = new MathLib(new ListFunctionStrategy());
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            Console.WriteLine(mathLib.SquareFactorial(200));
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.GetTimeString());
-
-          
+            MathLib mathLib = new MathLib(new ListFunctionStrategy());          
+            Console.WriteLine(mathLib.SquareFactorial(200));                    
             Console.ReadLine();
-        }
-
-      
+        }      
     }
 }
